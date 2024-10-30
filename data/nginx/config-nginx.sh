8175cd6a90190705f06291c010adc4d07e6ab086
Alexander <alex.tonkovid@gmail.com>
2024-10-30 18:16:58 +0100

    to fix  Error: local-exec provisioner error

---
+++ data/nginx/config-nginx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set variables
+CONFIG_SOURCE="/tmp/nginx.conf"
+CONFIG_DESTINATION="/etc/nginx/nginx.conf"
+
+echo "Starting NGINX configuration update"
+
+# Step 1: Copy the configuration file
+if sudo cp "$CONFIG_SOURCE" "$CONFIG_DESTINATION"; then
+    echo "Copied nginx.conf to $CONFIG_DESTINATION"
+else
+    echo "Copy failed"
+    exit 1
+fi
+
+# Step 2: Test the NGINX configuration
+if sudo nginx -t -c "$CONFIG_DESTINATION"; then
+    echo "NGINX configuration test passed"
+else
+    echo "NGINX configuration test failed"
+    exit 1
+fi
+
+# Step 3: Reload NGINX
+if sudo service nginx reload; then
+    echo "NGINX reload command executed successfully"
+else
+    echo "Failed to reload NGINX"
+    exit 1
+fi
+
+echo "NGINX configuration update completed"
---
+++ peovision.tf
@@ -0,0 +1,63 @@
+resource "terraform_data" "bootstrap-k3s" {
+  triggers_replace = [aws_instance.k3s_instance.id]
+
+  connection {
+    type                = "ssh"
+    user                = "ubuntu"
+    private_key         = file("~/.ssh/id_ed25519")
+    host                = aws_instance.k3s_instance.private_ip
+    bastion_host        = aws_eip.nat_eip.public_ip 
+    bastion_user        = "ec2-user"
+    bastion_private_key = file("~/.ssh/id_ed25519")
+  }
+
+  provisioner "file" {
+    source      = "data"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/data/bootstrap-k3s.sh",
+      "/tmp/data/bootstrap-k3s.sh args",
+    ]
+
+
+  }
+
+  provisioner "local-exec" {
+    command = "bash data/copy-kube-conf.sh"
+
+    environment = {
+      PRIVATE_INSTANCE_IP = aws_instance.k3s_instance.private_ip
+      BASTION_HOST_IP     = aws_eip.nat_eip.public_ip
+    }
+  }
+}
+
+resource "terraform_data" "bootstrap-bastion" {
+  triggers_replace = [aws_instance.nat_instance.id]
+
+  connection {
+    type                = "ssh"
+    user                = "ec2-user"
+    private_key         = file("~/.ssh/id_ed25519")
+    host                = aws_eip.nat_eip.public_ip
+  }
+
+  provisioner "file" {
+    source      = "data/nginx/"
+    destination = "/tmp"
+  }
+
+  
+
+  provisioner "local-exec" {
+  command = "ssh -tt ec2-user@$BASTION_HOST_IP \"sudo bash /tmp/config-nginx.sh\""
+
+      environment = {
+        BASTION_HOST_IP     = aws_eip.nat_eip.public_ip
+    }
+  }
+  
+}
